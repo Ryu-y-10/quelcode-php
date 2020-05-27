b41c69628303a03bc1d11d30b8e73955dadcfd07
Ryu-y-10 <ryu@RyunosukenoMacBook-Pro.local>
2020-05-28 00:29:28 +0900

    add: 要件動作を満たすスクリプト追加

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,8 +2,16 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
+$cnt = count($array);
 
+for ($i = 0; $i < $cnt - 1; $i++) {
+    for ($j = 0; $j < $cnt - 1 - $i; $j++) {
+        if ($array[$j] > $array[$j + 1]) {
+            $tmp = $array[$j + 1];
+            $array[$j + 1] = $array[$j];
+            $array[$j] = $tmp;
+        }
+    }
 }
 // 修正はここまで
 
