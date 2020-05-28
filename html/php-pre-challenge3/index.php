986e4a96d67b67929437ce8b4b15a09cfb1a6935
Ryu-y-10 <ryu@RyunosukenoMacBook-Pro.local>
2020-05-28 11:53:23 +0900

    add: DB接続処理追加

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -4,3 +4,11 @@ $limit = $_GET['target'];
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
+
+try {
+    $db = new PDO($dbn, $dbuser, $dbpassword);
+} catch (PDOException $e) {
+    echo 'DB接続エラー：';  $e->getMessage();
+    http_response_code(500);
+}
+
